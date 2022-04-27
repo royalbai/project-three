3d1b7fda32c90b5ec8d33b29469158a6bf06e73c
Royal <royalbai9@gmail.com>
2022-04-27 17:56:53 -0400

    First commit with react and firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAjLAIVPk_nRQPXOVfmlmPCa3XQmBcZ4gs",
+    authDomain: "project-3-fitness-planner.firebaseapp.com",
+    databaseURL: "https://project-3-fitness-planner-default-rtdb.firebaseio.com",
+    projectId: "project-3-fitness-planner",
+    storageBucket: "project-3-fitness-planner.appspot.com",
+    messagingSenderId: "481498483128",
+    appId: "1:481498483128:web:21fd8f03714ae38198a74d"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
