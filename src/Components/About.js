2b64289822393bacf657be6492d8a9166f55290f
Royal <royalbai9@gmail.com>
2022-05-03 12:01:19 -0400

    {

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ const About = () => {
     return(
         <div className="about">
             <h2>How to Get Started</h2>
-            <p>Plan your week ahead of time with the <strong>Fitness Planner</strong> app! Start by putting in the day of the week(Mon-Sun), then any form of excercise ranging from yoga to the gym to outdoor sports, and follow it up with the length of time you aim to workout for. Afterwards, when the week has ended you can clear the entries and log in your plans for the new week ahead of you. Achieve your goals or strive for a healthy lifetyle!</p>
+            <p>Plan your week ahead of time with the <strong>Fitness Planner</strong> app! Start by selecting the day of the week, then put in ANY form of excercise ranging from walking to yoga to the gym to outdoor sports, you name it!  Follow it up with the length of time you aim to workout for and you're all set. Afterwards, when the week has ended you can clear the entries and log in your new plans for the week ahead of you. Achieve your goals or strive for a healthy lifetyle!</p>
         </div>
     );
 }
